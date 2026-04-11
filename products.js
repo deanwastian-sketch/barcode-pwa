5c6f7e0e94efdf40ec6585e25e6c698c8b519d41
deanwastian-sketch <dean.wastian@gmail.com>
2026-04-11 14:01:00 +0200

    Create products.js

---
+++ products.js
@@ -0,0 +1,28 @@
+// products.js
+const products = {
+    "3830076490125": {
+        name: "Kreda Rumena",
+        desc: "Set 12 kosov",
+        warehouse: "Ljubljana",
+        stock: 20,
+        lastOrder: "2026-03-15",
+        hint: "Uporabite za tablo v učilnici."
+    },
+    "9876543210987": {
+        name: "Svinčnik HB",
+        desc: "Leseni, standardni",
+        warehouse: "Maribor",
+        stock: 100,
+        lastOrder: "2026-04-01",
+        hint: "Idealno za pisanje na papir."
+    },
+    // primer dodajanja novega artikla
+    "5556667778889": {
+        name: "Rdeča barva",
+        desc: "Akrilna, 250ml",
+        warehouse: "Celje",
+        stock: 35,
+        lastOrder: "2026-04-10",
+        hint: "Primerno za umetniške projekte."
+    }
+};
