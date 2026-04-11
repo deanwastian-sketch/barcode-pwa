ad005daa14120413a3bb392e58a384ae0d98a670
deanwastian-sketch <dean.wastian@gmail.com>
2026-04-11 22:25:56 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 const products = {
-    "3830076490125": {name:"Artikel A", desc:"Kratek opis A"},
-    "4334036092804": {name:"Artikel B", desc:"Kratek opis B"},
-    "4058143083745": {name:"Artikel C", desc:"Kratek opis C"},
-    "4056232161442": {name:"Artikel D", desc:"Kratek opis D"}
+  "3830076490125": { name: "Artikel A", desc: "Kratek opis A" },
+  "4334036092804": { name: "Artikel B", desc: "Kratek opis B" },
+  "4058143083745": { name: "Artikel C", desc: "Kratek opis C" },
+  "4056232161442": { name: "Artikel D", desc: "Kratek opis D" }
 };
