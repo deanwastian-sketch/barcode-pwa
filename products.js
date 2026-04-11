1a7652443e1ef71ea0657fc9f2b2996a14480b03
deanwastian-sketch <dean.wastian@gmail.com>
2026-04-11 22:42:41 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,12 @@
 const products = {
-  "3830076490125": { name: "Artikel A", desc: "Kratek opis A" },
-  "4334036092804": { name: "Artikel B", desc: "Kratek opis B" },
-  "4058143083745": { name: "Artikel C", desc: "Kratek opis C" },
-  "4056232161442": { name: "Artikel D", desc: "Kratek opis D" }
+  "3830001000006": { name: "Mleko 1L", desc: "Polnomastno mleko 3.5%" },
+  "3830001000013": { name: "Kruh bel", desc: "Svež beli kruh 500 g" },
+  "3830001000020": { name: "Jogurt sadni", desc: "Jagodni jogurt 180 g" },
+  "3830001000037": { name: "Maslo", desc: "Maslo 250 g" },
+  "3830001000044": { name: "Sir gauda", desc: "Poltrdi sir v kosu" },
+  "3830001000051": { name: "Jabolčni sok", desc: "100 % naravni sok 1 L" },
+  "3830001000068": { name: "Testenine", desc: "Špageti durum pšenica" },
+  "3830001000075": { name: "Riž", desc: "Dolgozrnati riž 1 kg" },
+  "3830001000082": { name: "Čokolada", desc: "Mlečna čokolada 100 g" },
+  "3830001000099": { name: "Kava mleta", desc: "Mleta kava 250 g" }
 };
