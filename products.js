f0e8dbcf70291ac81054eaf512fcf3679b42a2a6
deanwastian-sketch <dean.wastian@gmail.com>
2026-04-11 19:01:14 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 const products = {
-    "3830076490125": {name:"Artikel A", desc:"Kratek opis A", warehouse:"Skladišče 1", stock:"50", lastOrder:"2026-03-01", hint:"Namig za Artikel A"},
-    "4334036092804": {name:"Artikel B", desc:"Kratek opis B", warehouse:"Skladišče 2", stock:"20", lastOrder:"2026-03-05", hint:"Namig za Artikel B"},
-    "4056232161442": {name:"Artikel C", desc:"Kratek opis C", warehouse:"Skladišče 1", stock:"15", lastOrder:"2026-03-08", hint:"Namig za Artikel C"},
-    "4058143083745": {name:"Artikel D", desc:"Kratek opis D", warehouse:"Skladišče 3", stock:"5", lastOrder:"2026-03-10", hint:"Namig za Artikel D"}
+    "123456789012": {name:"Artikel A", desc:"Kratek opis A"},
+    "234567890123": {name:"Artikel B", desc:"Kratek opis B"},
+    "345678901234": {name:"Artikel C", desc:"Kratek opis C"},
+    "456789012345": {name:"Artikel D", desc:"Kratek opis D"}
 };
