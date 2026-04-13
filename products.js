4049caf44a3efd7fd0eade3c5644353781e57f36
deanwastian-sketch <dean.wastian@gmail.com>
2026-04-13 13:39:41 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,102 +1,102 @@
 const products = {
   "2002051976011": {
     naziv: "Artikel 1",
-    opis: "Testni artikel št. 1",
+    opis: "Precej uporabna zadeva kadar se želiš ogreti. Poznamo več izvedb - od precej močnih, do tistih povsem naravnih. Brez roka uporabe.",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "Dnevna soba",
+    Zaloga: "stalna, 4-5",
+    Zadnje_narocilo: "sredi decembra",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Je v različnih barvah - bela, tudi rjava."
   },
   "2002051976028": {
     naziv: "Artikel 2",
-    opis: "Testni artikel št. 2",
+    opis: "Zlasti nepogrešljivo, kadar je govora o Svenu. Občasno tudi o Deanu. Redko, če govorimo o Larisi. In skoraj zagotovo vedno, kadar govorimo o tebi. Rok uporabe: neznan",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "Likalnica",
+    Zaloga: 1,
+    Zadnje_narocilo: "november 2025",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Če to ne pomaga je pač za stran"
   },
   "2002051976035": {
     naziv: "Artikel 3",
-    opis: "Testni artikel št. 3",
+    opis: "Nahajam se v torbici, kopalnici, avtu in spalnici – a nikoli tam, kjer me iščeš. Rok trajanja: nekaj dni do nekaj tednov",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "povsod",
+    Zaloga: 2,
+    Zadnje_narocilo: "zadnji mesec",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Ko me imaš, sem samoumevna. Ko me nimaš, je panika."
   },
   "2002051976042": {
     naziv: "Artikel 4",
-    opis: "Testni artikel št. 4",
+    opis: "Ne kuham in ne jem. A kosila brez mene nikoli ni! Rok trajanja: nekaj mesecev",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "Kuhinja",
+    Zaloga: 4,
+    Zadnje_narocilo: "enkrat 2024",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Vročine se ne bojim"
   },
   "2002051976059": {
-    naziv: "Artikel 5",
-    opis: "Testni artikel št. 5",
+    naziv: "Artikel 5 - praline Lindt",
+    opis: "One točno vedo kaj pomeni sreča in zadovoljstvo. Rok trajanja: nekaj ur do max 2 dni",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "Spalnica",
+    Zaloga: 1,
+    Zadnje_narocilo: "2.4.2026",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Kroglice sreče"
   },
   "2002051976066": {
-    naziv: "Artikel 6",
-    opis: "Testni artikel št. 6",
+    naziv: "Artikel 6 - plinski gorilec",
+    opis: "Prav ti pridem v vseh letnih časih. Čeprav naju včasih heca, nama na koncu vedno uspe. Rok trajanja: kakšen mesec ali dva",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "Terasa",
+    Zaloga: 1,
+    Zadnje_narocilo: "februar 2025",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Ena tvojih ljubših igrač"
   },
   "2002051976073": {
-    naziv: "Artikel 7",
-    opis: "Testni artikel št. 7",
+    naziv: "Artikel 7 - čigumiji",
+    opis: "Vedno pri roki. V predalu, v avtu, v torbici. Rok trajanja: 1 mesec",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "avto",
+    Zaloga: "zelo niha",
+    Zadnje_narocilo: "prejšnji mesec",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Izjemno sveže"
   },
   "2002051976080": {
-    naziv: "Artikel 8",
-    opis: "Testni artikel št. 8",
+    naziv: "Artikel 8 - antistresna žogica",
+    opis: "Čutim tvojo energijo in ti jo pomagam usmerit v pravo smer. Manjša kot tvoja dlan, pa vendar večja od vseh tvojih skrbi. Rok trajanja: neomejeno",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "Pisarna",
+    Zaloga: 0,
+    Zadnje_narocilo: "ni podatka",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Stres noter - mir ven"
   },
   "2002051976097": {
     naziv: "Artikel 9",
-    opis: "Testni artikel št. 9",
+    opis: "Kaj si nor kako gre če me kdo kak dan ne prime v roke. Da si drznejo me tam pustit in ležat. Nesprejemljivo! Rok trajanja: cca 6 mesecev",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "Kopalnica",
+    Zaloga: 1,
+    Zadnje_narocilo: "2025",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Sven, kr idi nazaj!"
   },
   "2002051976103": {
     naziv: "Artikel 10",
-    opis: "Testni artikel št. 10",
+    opis: "Brez mene ti živeti ni. Še kavo lažje pogrešiš. V vseh barvah in vonjih. Rok trajanja: 10-14 dni",
 
-    skladisce: "Centralno skladišče Ljubljana",
-    Zaloga: 12,
-    Zadnje_narocilo: "15.03.2026",
+    skladisce: "avto",
+    Zaloga: "2+",
+    Zadnje_narocilo: "vsak mesec",
 
-    Namig: "Pomaga pri hladnem vremenu"
+    Namig: "Jasno, vse jasno!"
   }
 };
