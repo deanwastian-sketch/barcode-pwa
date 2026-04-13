0f0c6ed2485c0cef99d970e0e43e729f36030e1d
deanwastian-sketch <dean.wastian@gmail.com>
2026-04-13 11:29:49 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,62 +1,102 @@
 const products = {
   "2002051976011": {
-    name: "Artikel 1",
-    desc: "Testni artikel št. 1",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 01"
+    naziv: "Artikel 1",
+    opis: "Testni artikel št. 1",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976028": {
-    name: "Artikel 2",
-    desc: "Testni artikel št. 2",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 02"
+    naziv: "Artikel 2",
+    opis: "Testni artikel št. 2",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976035": {
-    name: "Artikel 3",
-    desc: "Testni artikel št. 3",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 03"
+    naziv: "Artikel 3",
+    opis: "Testni artikel št. 3",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976042": {
-    name: "Artikel 4",
-    desc: "Testni artikel št. 4",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 04"
+    naziv: "Artikel 4",
+    opis: "Testni artikel št. 4",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976059": {
-    name: "Artikel 5",
-    desc: "Testni artikel št. 5",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 05"
+    naziv: "Artikel 5",
+    opis: "Testni artikel št. 5",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976066": {
-    name: "Artikel 6",
-    desc: "Testni artikel št. 6",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 06"
+    naziv: "Artikel 6",
+    opis: "Testni artikel št. 6",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976073": {
-    name: "Artikel 7",
-    desc: "Testni artikel št. 7",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 07"
+    naziv: "Artikel 7",
+    opis: "Testni artikel št. 7",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976080": {
-    name: "Artikel 8",
-    desc: "Testni artikel št. 8",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 08"
+    naziv: "Artikel 8",
+    opis: "Testni artikel št. 8",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976097": {
-    name: "Artikel 9",
-    desc: "Testni artikel št. 9",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 09"
+    naziv: "Artikel 9",
+    opis: "Testni artikel št. 9",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   },
   "2002051976103": {
-    name: "Artikel 10",
-    desc: "Testni artikel št. 10",
-    category: "Test",
-    note: "Datum 02.05.1976 – zap. 10"
+    naziv: "Artikel 10",
+    opis: "Testni artikel št. 10",
+
+    skladisce: "Centralno skladišče Ljubljana",
+    Zaloga: 12,
+    Zadnje_narocilo: "15.03.2026",
+
+    Namig: "Pomaga pri hladnem vremenu"
   }
 };
